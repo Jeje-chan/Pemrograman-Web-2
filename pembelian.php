79d0f21b51ff02ed755d926d015430fa748c07f2
Sharma Azzahrah <114566864+Jeje-chan@users.noreply.github.com>
2023-04-09 15:08:35 +0700

    Week 6

--- pembelian.php
+++ pembelian.php
@@ -13,7 +13,7 @@ $data_pembelian = $model->dataPembelian();
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="index.php?hal=home">Home</a></li>
-              <li class="breadcrumb-item active">Produk</li>
+              <li class="breadcrumb-item active">Pembelian</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
