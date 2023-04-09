71f902e56815e60e244f8205a72026a48029a764
Sharma Azzahrah <114566864+Jeje-chan@users.noreply.github.com>
2023-04-09 15:06:56 +0700

    Update pembayaran.php

--- pembayaran.php
+++ pembayaran.php
@@ -13,7 +13,7 @@ $data_pembayaran = $model->dataPembayaran();
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="index.php?hal=home">Home</a></li>
-              <li class="breadcrumb-item active">Produk</li>
+              <li class="breadcrumb-item active">Pembayaran</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
