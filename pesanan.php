2e89432bcc3f4d24b37a1e8e5436ec143b457534
Sharma Azzahrah <114566864+Jeje-chan@users.noreply.github.com>
2023-04-09 15:09:05 +0700

    Week 6

--- pesanan.php
+++ pesanan.php
@@ -13,7 +13,7 @@ $data_pesanan = $model->dataPesanan();
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="index.php?hal=home">Home</a></li>
-              <li class="breadcrumb-item active">Produk</li>
+              <li class="breadcrumb-item active">Pesanan</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
